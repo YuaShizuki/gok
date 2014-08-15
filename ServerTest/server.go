ae2a353303852d875a96ec5cebeb62e32a3381c8
Yua <sumodrop@gmail.com>
2014-08-15 02:24:26 -0700

    standard

--- ServerTest/server.go
+++ ServerTest/server.go
@@ -1,21 +1,42 @@
 package main
 import "fmt"
 import "net/http"
+import "io/ioutil"
 
 /*type handel func(*Gok);
 var routes map[string]handel;*/
 
-type mainHandler struct{};
-func (_ *mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
     var html string =
 `
 <html>
     <body>
-        <h1>Die Inside Me,...,</h1>
+        <h1>Die Inside Me!</h1>
+        <form enctype='multipart/form-data' name="myGorm" action="/postit" method="POST">
+            First name: <input type="text" name="firstname"><br>
+            Last name: <input type="text" name="lastname">
+            File: <input type="file" name="content"> 
+            <input type="submit" value="Send">
+        </form>
     </body>
 </html>
-`
+`;
     fmt.Fprintln(w, html);
+}
+
+func postit(w http.ResponseWriter, r *http.Request) {
+    f,_,_ := r.FormFile("content");
+    file,_ := ioutil.ReadAll(f);
+    fmt.Fprintln(w, string(file));
+}
+
+type mainHandler struct{};
+func (_ *mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+   if r.URL.Path == "/" {
+       index(w, r);
+   } else if r.URL.Path == "/postit" {
+       postit(w, r);
+   }
 }
 
 func main() {
