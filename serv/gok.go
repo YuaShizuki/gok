e14340dd89a629ddd7467d0b806cccbe4bc001dc
Yua <sumodrop@gmail.com>
2014-08-15 23:45:22 -0700

    standard

---
+++ serv/gok.go
@@ -0,0 +1,124 @@
+package main
+import "fmt"
+import "net/http"
+import "time"
+import "strings"
+import "os"
+import "io"
+
+type Gok struct {
+    w http.ResponseWriter;
+    r *http.Request;
+};
+
+func (self *Gok) Echo(a ...interface{}) {
+    fmt.Fprint(self.w, a...);
+}
+/*- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $_SERVER <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
+func (self *Gok) ServerSelf() string {
+    return self.r.URL.Path;
+}
+func (self *Gok) ServerHttpUserAgent() string {
+    return strings.Join(self.r.Header["User-Agent"], "\n");
+}
+func (self *Gok) ServerHttpReferer() string {
+    return strings.Join(self.r.Header["Referer"], "\n");
+}
+
+func (self *Gok) ServerHttps() bool {
+    return self.r.URL.Scheme == "https";
+}
+func (self *Gok) ServerRemoteAddr() string {
+    return strings.Split(self.r.RemoteAddr, ":")[0];
+}
+func (self *Gok) ServerRemotePort() string {
+    return strings.Split(self.r.RemoteAddr, ":")[1];
+}
+func (self *Gok) ServerPort() int {
+    return 80;
+}
+func (self *Gok) ServerHttpAcceptEncoding() string {
+    return strings.Join(self.r.Header["Accept-Encoding"], "\n");
+}
+func (self *Gok) ServerProtocol() string {
+    return self.r.Proto;
+}
+func (self *Gok) ServerRequestMethod() string {
+    return self.r.Method;
+}
+func (self *Gok) ServerQueryString() string {
+    return self.r.URL.RawQuery
+}
+func (self *Gok) ServerHttpAccept() string {
+    return strings.Join(self.r.Header["Accept"], "\n");
+}
+func (self *Gok) ServerHttpAcceptCharset() string {
+    return strings.Join(self.r.Header["Accept-Charset"], "\n");
+}
+func (self *Gok) ServerHttpAcceptLanguage() string {
+    return strings.Join(self.r.Header["Accept-Language"], "\n");
+}
+func (self *Gok) ServerHttpConnection() string {
+    return strings.Join(self.r.Header["Connection"], "\n");
+}
+func (self *Gok) ServerHttpHost() string {
+    return self.r.Host;
+}
+
+
+/*- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $_GET && $_POST <<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
+
+func (self *Gok) Post(name string) []byte {
+    return nil;
+}
+func (self *Gok) Get(name string) string {
+    return "";
+}
+
+/*- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  $_COOKIE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
+func (self *Gok) Cookie(name string) string {
+    return "";
+}
+func (self *Gok) SetCookie(name string, value string, expires *time.Time) {
+}
+func (self *Gok) SetCookie_4(name string, value string, expires *time.Time, path string){
+}
+func (self *Gok) SetCookie_5(name string, value string, expires *time.Time, path string,
+                            domain string){
+}
+func (self *Gok) SetCookie_7(name string, value string, expires *time.Time, path string,
+                            domain string, secure bool, httpOnly bool) {
+}
+func (self *Gok) UnSetCookie(name string) {
+}
+
+/*- $_FILE -*/
+func (self *Gok) File(name string) (string, string, string, int64) {
+    f, fHeader, err := self.r.FormFile(name);
+    if err != nil {
+        return "", "", "", 0;
+    }
+    defer f.Close();
+    fileName := genRandName();
+    f2, err := os.Create(fileName);
+    if err != nil {
+        return "", "", "", 0;
+    }
+    defer f2.Close();
+    size, err := io.Copy(f2, f);
+    if err != nil {
+        return "", "", "", 0;
+    }
+    if len(fHeader.Header["Content-Type"]) == 0 {
+        return fileName, fHeader.Filename, "", size;
+    }
+    return fileName, fHeader.Filename, fHeader.Header["Content-Type"][0], size;
+}
+
+/*- Headers -*/
+func (self *Gok) RequestHeader() map[string]string { return nil; }
+func (self *Gok) ResponseHeader() map[string]string { return nil; }
+
+/*- Request/Writer -*/
+func (self *Gok) RequestWriter() http.ResponseWriter { return self.w; }
+func (self *Gok) HttpRequest() *http.Request { return self.r; }
---
+++ serv/server.go
@@ -0,0 +1,71 @@
+package main
+import "fmt"
+import "net/http"
+import "io"
+import "os"
+import "math/rand"
+import "encoding/hex"
+import "time"
+/*type handel func(*Gok);
+var routes map[string]handel;*/
+
+func index(w http.ResponseWriter, r *http.Request) {
+    var html string =
+`
+<html>
+    <body>
+        <h1>Die Inside Me!</h1>
+        <form enctype='multipart/form-data' name="myGorm" action="/postit" method="POST">
+            First name: <input type="text" name="firstname"><br>
+            Last name: <input type="text" name="lastname">
+            File: <input type="file" name="content"> 
+            <input type="submit" value="Send">
+        </form>
+    </body>
+</html>
+`;
+    fmt.Fprintln(w, html);
+}
+
+func genRandName() string {
+    rand.Seed(time.Now().UnixNano());
+    num := rand.Uint32();
+    str := []byte{ byte(num), byte(num >> 8), byte(num >> 16), byte(num >> 24) };
+    return hex.EncodeToString(str);
+}
+
+func postit(w http.ResponseWriter, r *http.Request) {
+    f, _, err := r.FormFile("content");
+    if err != nil {
+        fmt.Fprintln(w, err);
+        return;
+    }
+    defer f.Close();
+    name := genRandName();
+    f2, err := os.Create(name);
+    if err != nil {
+        fmt.Fprintln(w, err);
+        return;
+    }
+    defer f2.Close();
+    io.Copy(f2, f);
+    fmt.Fprintln(w, name);
+}
+
+type mainHandler struct{};
+func (_ *mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+   if r.URL.Path == "/" {
+       index(w, r);
+   } else if r.URL.Path == "/postit" {
+       postit(w, r);
+   }
+}
+
+func main() {
+    http.Handle("/", new(mainHandler))
+    fmt.Println("server running like a bitch!");
+    err := http.ListenAndServe(":8080", nil);
+    if err != nil {
+        fmt.Println(err);
+    }
+}
