263f0d331c0dd53b9d6e32e8e487eaad7157a238
Yua <sumodrop@gmail.com>
2014-08-23 18:49:23 -0700

    standard

--- proc.go
+++ proc.go
@@ -1,91 +1,92 @@
 package main
+
 import "fmt"
 import "strings"
 import "errors"
 
 func processGokContent(code string) (string, string, error) {
-    var gofile string = "%s\nfunc %s(gok *Gok){%s\n}";
-    funcName := fmt.Sprintf("Render%s", genRandName());
-    imports, r, err := buildImports(code);
-    if err != nil {
-        return "", "", err;
-    }
-    goCode, err := buildGoCode(code[r:]);
-    if err != nil {
-        return "", "", err;
-    }
-    final := fmt.Sprintf(gofile, imports , funcName, goCode);
-    return final, funcName, nil;
+	var gofile string = "%s\nfunc %s(gok *Gok){%s\n}"
+	funcName := fmt.Sprintf("Render%s", genRandName())
+	imports, r, err := buildImports(code)
+	if err != nil {
+		return "", "", err
+	}
+	goCode, err := buildGoCode(code[r:])
+	if err != nil {
+		return "", "", err
+	}
+	final := fmt.Sprintf(gofile, imports, funcName, goCode)
+	return final, funcName, nil
 }
 
 func buildImports(code string) (string, int, error) {
-    p := "<?go-imports"
-    pe := "?>";
-    plen := len(p);
-    indx := strings.Index(code, p);
-    if indx == -1 {
-        return "", 0, nil;
-    }
-    indxEnd := strings.Index(code, pe);
-    if indxEnd == -1 {
-        return "", -1, errors.New("unknown code pattern");
-    }
-    if indxEnd == (indx + plen) {
-        return "", -1, nil;
-    }
-    imports := strings.Split(code[(indx+plen):indxEnd], "\n");
-    for i := range imports {
-        imports[i] = strings.TrimSpace(imports[i]);
-    }
-    return "package main\n"+strings.Join(imports, "\n"), (indxEnd+2), nil;
+	p := "<?go-imports"
+	pe := "?>"
+	plen := len(p)
+	indx := strings.Index(code, p)
+	if indx == -1 {
+		return "", 0, nil
+	}
+	indxEnd := strings.Index(code, pe)
+	if indxEnd == -1 {
+		return "", -1, errors.New("unknown code pattern")
+	}
+	if indxEnd == (indx + plen) {
+		return "", -1, nil
+	}
+	imports := strings.Split(code[(indx+plen):indxEnd], "\n")
+	for i := range imports {
+		imports[i] = strings.TrimSpace(imports[i])
+	}
+	return "package main\n" + strings.Join(imports, "\n"), (indxEnd + 2), nil
 }
 
 func buildGoCode(code string) (string, error) {
-    echoFunc := "\ngok.Echo(\"%s\");";
-    p := "<?go ";
-    pe := "?>";
-    codeLen := len(code);
-    result := make([]byte, 0, codeLen*3);
-    for last := 0; last < codeLen; {
-        slice := code[last:];
-        indx := strings.Index(slice, p);
-        if indx == -1 {
-            if len(slice) > 0 {
-                echo := fmt.Sprintf(echoFunc, strToCStr(slice));
-                result = append(result, echo...);
-            }
-            break;
-        }
-        if indx != 0 {
-            echo := fmt.Sprintf(echoFunc, strToCStr(slice[0:indx]))
-            result = append(result, echo...);
-        }
-        indxEnd := strings.Index(slice, pe);
-        if indxEnd == -1 {
-            return "", errors.New("unknown code pattern");
-        }
-        if indxEnd == (indx+5) {
-            last += indxEnd+2;
-            continue;
-        }
-        goCode := strings.TrimSpace(slice[indx+5:indxEnd]);
-        if len(goCode) != 0 {
-            for _,v := range strings.Split(goCode, "\n") {
-                result = append(result, '\n');
-                result = append(result, strings.TrimSpace(v)...);
-            }
-        }
-        last += indxEnd+2;
-    }
-    return string(result), nil;
+	echoFunc := "\ngok.Echo(\"%s\");"
+	p := "<?go "
+	pe := "?>"
+	codeLen := len(code)
+	result := make([]byte, 0, codeLen*3)
+	for last := 0; last < codeLen; {
+		slice := code[last:]
+		indx := strings.Index(slice, p)
+		if indx == -1 {
+			if len(slice) > 0 {
+				echo := fmt.Sprintf(echoFunc, strToCStr(slice))
+				result = append(result, echo...)
+			}
+			break
+		}
+		if indx != 0 {
+			echo := fmt.Sprintf(echoFunc, strToCStr(slice[0:indx]))
+			result = append(result, echo...)
+		}
+		indxEnd := strings.Index(slice, pe)
+		if indxEnd == -1 {
+			return "", errors.New("unknown code pattern")
+		}
+		if indxEnd == (indx + 5) {
+			last += indxEnd + 2
+			continue
+		}
+		goCode := strings.TrimSpace(slice[indx+5 : indxEnd])
+		if len(goCode) != 0 {
+			for _, v := range strings.Split(goCode, "\n") {
+				result = append(result, '\n')
+				result = append(result, strings.TrimSpace(v)...)
+			}
+		}
+		last += indxEnd + 2
+	}
+	return string(result), nil
 }
 
 func strToCStr(str string) string {
-    result := strings.Replace(str, "\n", "\\n", -1);
-    result = strings.Replace(result, "\t", "\\t", -1);
-    result = strings.Replace(result, "\r", "\\r", -1);
-    result = strings.Replace(result, "\v", "\\v", -1);
-    result = strings.Replace(result, "\f", "\\f", -1);
-    result = strings.Replace(result, "\"", "\\\"", -1);
-    return result;
+	result := strings.Replace(str, "\n", "\\n", -1)
+	result = strings.Replace(result, "\t", "\\t", -1)
+	result = strings.Replace(result, "\r", "\\r", -1)
+	result = strings.Replace(result, "\v", "\\v", -1)
+	result = strings.Replace(result, "\f", "\\f", -1)
+	result = strings.Replace(result, "\"", "\\\"", -1)
+	return result
 }
--- utility.go
+++ utility.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 import "os"
 import "encoding/hex"
@@ -6,22 +7,21 @@ import "math/rand"
 import "time"
 
 func pathExist(p string) bool {
-    _, err := os.Stat(p)
-    return err == nil;
+	_, err := os.Stat(p)
+	return err == nil
 }
 
 func errExit(e error, msg string) {
-    fmt.Println("error =>", e.Error());
-    if msg != "" {
-        fmt.Println(msg);
-    }
-    os.Exit(1);
+	fmt.Println("error =>", e.Error())
+	if msg != "" {
+		fmt.Println(msg)
+	}
+	os.Exit(1)
 }
 
 func genRandName() string {
-    rand.Seed(time.Now().UnixNano());
-    num := rand.Uint32();
-    str := []byte{ byte(num), byte(num >> 8), byte(num >> 16), byte(num >> 24) };
-    return hex.EncodeToString(str);
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Uint32()
+	str := []byte{byte(num), byte(num >> 8), byte(num >> 16), byte(num >> 24)}
+	return hex.EncodeToString(str)
 }
-
