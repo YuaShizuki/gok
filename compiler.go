889d336239f04ec5add58cc61468191b72c54fcd
Yua <sumodrop@gmail.com>
2014-08-24 19:47:32 -0700

    standard

--- compiler.go
+++ compiler.go
@@ -4,6 +4,7 @@ import "strings"
 import "io/ioutil"
 import "bytes"
 import "errors"
+import "container/list"
 
 func main() {
     f, err := ioutil.ReadFile("i.gok")
@@ -18,13 +19,33 @@ func main() {
 
 func compile(code string) (string, string, error) {
     lines := strings.Split(code, "\n")
-    ignoreindx := make(map[int]bool);
-    header, err := extract("<?gohead", lines, ignoreindx)
-    code, err := compileRenderer(lines, ignoreindx)
-    return header, "", err
+    processed := list.New()
+    header, err := extract("<?gohead", lines, takenIndxs)
+    if err != nil { return "", "", err }
+    funcs, err := extract("<?gofunc", lines, takenIndxs)
+    if err != nil { return "", "", nil }
+    code, err := compileRenderer(lines, takenIndxs)
+    if err != nil { return "", "", nil }
+    return header, "salvadorDali", err
 }
 
-func extract(pattern string, lines []string) (string, error) {
+type analyzed struct {
+    ln int
+    col1 int
+    col2 int
+}
+
+func containsln(processed *list.List, ln int) (col1, col2 int) {
+    for e := processed.Front; e != nil; e = e.Next() {
+        lnsAndCols := e.Value.(*analyzed)
+        if lnsAndCols.ln == ln {
+            return lnsAndCols.col1, lnsAndCols.col2
+        }
+    }
+    return -1, -1
+}
+
+func extract(pattern string, lines []string, processed *list.List) (string, error) {
     gocode := new(bytes.Buffer)
     plen := len(pattern)
     inside := false
@@ -38,25 +59,47 @@ func extract(pattern string, lines []string) (string, error) {
             if indx == -1 {
                 continue
             }
-            inside = true;
+            inside = true
+            lnsAndCols := new(analyzed)
+            lnsAndCols.ln = i
             if (lenl == plen) || (strings.TrimSpace(l[indx+plen:]) == "") {
+                lnsAndCols.col1 = 0
+                lnsAndCols.col2 = lenl
+                processed.PushBack(lnsAndCols)
                 continue
             }
             fmt.Fprintf(gocode, "//%d\n%s\n",(i+1), strings.TrimSpace(l[indx+plen:]))
+            lnsAndCols.col1 = indx+plen
+            lnsAndCols.col2 = lenl
+            processed.PushBack(lnsAndCols)
         } else {
+            lnsAndCols := new(analyzed)
+            lnsAndCols.ln = i
             indx := strings.Index(l, "?>")
             if indx == -1 {
                 fmt.Fprintf(gocode, "//%d\n%s\n", (i+1), strings.TrimSpace(l))
+                lnsAndCols.col1 = 0
+                lnsAndCols.col2 = lenl
+                processed.PushBack(lnsAndCols)
                 continue
             }
             inside = false;
             if lenl == 2 {
+                lnsAndCols.col1 = 0
+                lnsAndCols.col2 = lenl
+                processed.PushBack(lnsAndCols)
                 continue
             }
             if (indx == 0) || (strings.TrimSpace(l[:indx]) == "") {
+                lnsAndCols.col1 = 0
+                lnsAndCols.col1 = lenl
+                processed.PushBack(lnsAndCols)
                 continue
             }
             fmt.Fprintf(gocode, "//%d\n%s\n", (i+1), strings.TrimSpace(l[:indx]))
+            lnsAndCols.col1 = 0
+            lnsAndCols.col2 = indx
+            processed.PushBack(lnsAndCols)
         }
     }
     if inside {
@@ -65,8 +108,10 @@ func extract(pattern string, lines []string) (string, error) {
     return gocode.String(), nil
 }
 
-func compileRenderer(lines []string, skipNodes []string) (string, error) {
+func compileRenderer(lines []string, processed *list.List) (string, error) {
     gocode := new(bytes.Buffer)
-    insideSkip := false
+    inside := false
+    for i, l := range lines {
+    }
 }
 
