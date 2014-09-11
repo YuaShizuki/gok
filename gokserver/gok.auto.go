46977a80efb110c760960bf0bbae262e9d08c956
Yua <sumodrop@gmail.com>
2014-09-10 20:14:25 -0700

    standard

--- gokserver/gok.auto.go
+++ gokserver/gok.auto.go
@@ -8,9 +8,8 @@ import "os"
 import "io"
 import "bytes"
 
-
-
-//core struct encapsulates the http.Request and http.ResponseWriter 
+// core struct encapsulates the http.Request and http.ResponseWriter. 
+// instance present in all <?go ?> tags as `gok`
 type Gok struct {
     w http.ResponseWriter
     r *http.Request
@@ -20,44 +19,38 @@ type Gok struct {
     shouldRedirect bool
 };
 
-//equivalent to php `echo`, echo accepts any type of parameters
+// equivalent to php `echo`. echo accepts any type of parameters
 func (self *Gok) Echo(a ...interface{}) {
     if self.response != nil {
         fmt.Fprint(self.response, a...)
     }
 }
 
-//redirects the request to a new url 
+// redirects the request to a new url 
 func (self *Gok) Redirect(newUrl string) {
     self.shouldRedirect = true
     self.Header("Location:"+newUrl)
     self.w.WriteHeader(http.StatusMovedPermanently)
 }
 
-//Die sends the param msg as error, and undoes everything echoed so far.
+// Die sends the `msg` as error, and undoes everything echoed so far.
 func (self *Gok) Die(msg string) {
     self.response.Reset()
     self.response = nil
     self.deadMsg = msg
 }
 
-//=>                    PHP equivalent of $_SERVER[] functions.
-//returns the current request url without the leading `/`
+//=> PHP equivalent of $_SERVER[] functions.
+// returns the current request url without the leading `/`
 func (self *Gok) ServerSelf() string {
     return self.r.URL.Path[1:]
 }
-
-//user agent
 func (self *Gok) ServerHttpUserAgent() string {
     return self.r.Header.Get("User-Agent")
 }
-
-//http referer
 func (self *Gok) ServerHttpReferer() string {
     return self.r.Referer()
 }
-
-//if request is http
 func (self *Gok) ServerHttps() bool {
     return self.r.URL.Scheme == "https"
 }
@@ -99,11 +92,13 @@ func (self *Gok) ServerHttpHost() string {
 }
 
 
-/*- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $_GET && $_POST <<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
-
+//=> PHP equivalent of $_GET[''] and $_POST['']
+// parses the post request and returns the post value of `name`,
+// in case for post containing file date. use gok.File('fileName')
 func (self *Gok) Post(name string) string {
     return self.r.PostFormValue(name)
 }
+// the same as above but for get requests.
 func (self *Gok) Get(name string) string {
     if self.getValues == nil {
         self.getValues = self.r.URL.Query()
@@ -111,7 +106,9 @@ func (self *Gok) Get(name string) string {
     return self.getValues.Get(name)
 }
 
-/*- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  $_COOKIE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
+//=> PHP equivalent of $_COOKIE['']
+// returns the cookie value for `name`, if no cookie is set returns an
+// empty string ""
 func (self *Gok) Cookie(name string) string {
     if cookie, err := self.r.Cookie(name); err == http.ErrNoCookie {
         return ""
@@ -120,7 +117,8 @@ func (self *Gok) Cookie(name string) string {
     }
     return ""
 }
-
+// sets cookie with name, value, and duration to expire, if duration is 0
+// cookie is permanant
 func (self *Gok) SetCookie(name string, value string, duration int64) {
     if (len(name) == 0) || (len(value) == 0) {
         return
@@ -133,7 +131,7 @@ func (self *Gok) SetCookie(name string, value string, duration int64) {
     }
     http.SetCookie(self.w, cookie)
 }
-
+//delets the cookie set with `name`
 func (self *Gok) DeleteCookie(name string) {
     self.SetCookie(name, "deleted", -1000)
 }
@@ -154,6 +152,7 @@ func (self *Gok) SetCookie_4(name string, value string, duration int64,
     }
     http.SetCookie(self.w, cookie)
 }
+
 func (self *Gok) SetCookie_5(name string, value string, duration int64,
                                 urlPath string, domain string) {
     if (len(name) == 0) || (len(value) == 0) {
@@ -173,6 +172,7 @@ func (self *Gok) SetCookie_5(name string, value string, duration int64,
     }
     http.SetCookie(self.w, cookie)
 }
+
 func (self *Gok) SetCookie_7(name string, value string, duration int64,
                                 urlPath string, domain string, secure bool,
                                 httpOnly bool) {
@@ -196,7 +196,11 @@ func (self *Gok) SetCookie_7(name string, value string, duration int64,
     http.SetCookie(self.w, cookie)
 }
 
-/*- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $_FILE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
+//=> PHP equivalent of $FILE['']
+// File saves the file upload to disk and returns 
+// (`file path`,`uploaded file name`, `file content type`, `size`) of the file.
+// its important that file uploads occure from form with 
+// enctype='multipart/form-data' set.
 func (self *Gok) File(name string) (string, string, string, int64) {
     f, fHeader, err := self.r.FormFile(name)
     if err != nil {
@@ -219,7 +223,9 @@ func (self *Gok) File(name string) (string, string, string, int64) {
     return fileName, fHeader.Filename, fHeader.Header["Content-Type"][0], size
 }
 
-/*->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Header <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
+//=> PHP equivalent of $_HEADER['']
+// sets the header for response.
+// ex: gok.Header("Connection:Close")
 func (self *Gok) Header(header string) {
     h := strings.Split(header, ":")
     if len(h) != 2 {
@@ -227,15 +233,16 @@ func (self *Gok) Header(header string) {
     }
     self.w.Header().Set(h[0], h[1])
 }
-
+//go htt.Header for request
 func (self *Gok) RequestHeader() http.Header {
     return self.r.Header
 }
-
+//go http.Header for Response
 func (self *Gok) ResponseHeader() http.Header {
     return self.w.Header()
 }
 
-/*- >>>>>>>>>>>>>>>>>>>>>>>>>>>>> Request | Writer <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -*/
+// htt.ResponseWriter for gok object
 func (self *Gok) ResponseWriter() http.ResponseWriter { return self.w }
+// *http.Request for gok object
 func (self *Gok) HttpRequest() *http.Request { return self.r }
